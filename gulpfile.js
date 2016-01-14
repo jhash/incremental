2c07f3403f296901275f7cf9c631e1ffe0311021
Jacob Hash <jhash@umich.edu>
2016-01-13 19:06:30 -0500

    Added live reload and using gulp for web server

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ var
 gulp = require('gulp'),
 gulp_jspm = require('gulp-jspm'),
 batch = require('gulp-batch'),
+webserver = require('gulp-webserver'),
 watch = require('gulp-watch');
+
+gulp.task('webserver', function() {
+  gulp.src('')
+    .pipe(webserver({
+      livereload: true
+    }));
+});
 
 gulp.task('build', function() {
   return gulp.src('lib/main.js')
